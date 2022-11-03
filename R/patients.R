ea9de3f2b804206c41c49f56e0d764c42bd95e40
jerryhe <jerryyidihe@gmail.com>
2022-11-03 10:38:53 -0700

    Added functionatliy to suare plus some other stuff

---
+++ R/patients.R
@@ -0,0 +1,13 @@
+#' Toy Patient Data
+#'
+#' Some fake data on fake patients that I real made up
+#'
+#' @format ## `who`
+#' A data frame with 3 rows and 2 columns:
+#' \describe{
+#'   \item{id}{patient id}
+#'   \item{age}{patient age}
+#'   ...
+#' }
+#' @source Lucy's head
+"patients"
--- tests/testthat/test-square.R
+++ tests/testthat/test-square.R
@@ -2,6 +2,8 @@ test_that("Basically functionality test", {
   expect_equal(square(3), 9)
   expect_equal(square(1:3), c(1, 4, 9))
 })
+expect_equal(square(as.list(c(2, 4))), c(4, 16))
+
 
 
 
